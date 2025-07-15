c8af7acdbb85e97e7c7260be65b38ce19c814979
FikriAnwar0 <FikriAnwar0@users.noreply.github.com>
2025-07-15 19:12:38 +0700

    Add package.json and basic Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+  // Daftarkan task default kosong
+  grunt.registerTask('default', []);
+};
