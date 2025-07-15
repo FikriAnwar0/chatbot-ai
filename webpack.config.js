949ca66ec54a2a4ab6f441dcfdca7f5377ce390a
FikriAnwar0 <FikriAnwar0@users.noreply.github.com>
2025-07-15 19:43:37 +0700

    update error

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './main.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  mode: "production"
+  mode: 'production'
 };
