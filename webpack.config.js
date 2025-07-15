5ff3863721ccad12a5daa47b4eee8cae2f6c8ea8
FikriAnwar0 <FikriAnwar0@users.noreply.github.com>
2025-07-15 19:51:56 +0700

    update: setup vercel.json and dist output

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
 
 module.exports = {
-  entry: './main.js',
+  entry: './main.js', // Tetap pakai main.js karena kamu pakai itu di root
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true, // hapus otomatis file lama di dist saat build baru
   },
-  mode: 'production'
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'], // kalau pakai CSS import
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // opsional, kalau pakai ES6
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  }
 };
